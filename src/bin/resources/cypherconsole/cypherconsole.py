7fc8669b40f8083c1126fe6dc1896f670a2f13c3
Anders Nawroth <anders@neotechnology.com>
2012-03-23 18:18:18 +0100

    Made the database parameter optional.

--- src/bin/resources/cypherconsole/cypherconsole.py
+++ src/bin/resources/cypherconsole/cypherconsole.py
@@ -3,30 +3,35 @@
 
 import sys
 
-title = ''
 if len(sys.argv) > 1:
   active = sys.argv[1].startswith('1')
-for i in [2, 3]:
-  if len(sys.argv) > i:
-    key,value = sys.argv[i].split('=')
-    if key == 'title':
-      title = value
-    elif key == 'db':
-      db = value
 
-if active:
+if not active:
+  sys.stdout.write(' ')
+else:
+  title = ''
+  db = ''
+  for i in [2, 3]:
+    if len(sys.argv) > i:
+      key,value = sys.argv[i].split('=')
+      if key == 'title':
+        title = value
+      elif key == 'db':
+        db = value
   data = sys.stdin.readlines()
-  query = data.pop(0) 
+  if len(data) > 0:
+    query = data.pop(0) 
   body = []
-  if len(title) > 1:
+  if len(title) > 0:
     body.append('<formalpara role="cypherconsole"><title>')
     body.append(title)
     body.append('</title><para>')
   else:
     body.append('<simpara role="cypherconsole">')
-  body.append('<database>')
-  body.append(db)
-  body.append('</database>')
+  if len(db) > 0:
+    body.append('<database>')
+    body.append(db)
+    body.append('</database>')
   body.append('<command>')
   body.append(query)
   body.append('</command>')
@@ -35,6 +40,4 @@ if active:
   else:
     body.append('</simpara>')
   sys.stdout.write(''.join(body))
-else:
-  sys.stdout.write(' ')
   
